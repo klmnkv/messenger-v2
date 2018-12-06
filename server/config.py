f630373921b3e3edab55a25ed83201ce4350b320
klmnkv <39479881+klmnkv@users.noreply.github.com>
2018-12-06 20:06:43 +0700

    Add files via upload

---
+++ server/config.py
@@ -0,0 +1,9 @@
+server_ip = '172.20.10.3'
+server_port = 8080
+server_addr = (server_ip, server_port)
+
+database = 'kolomnikov_db'
+db_user = 'kolomnikovdb'
+db_password = 'kolomnikov'
+db_host = 'localhost'
+db_port = '5432'
